3a28d8f297492280c78d902364b017bf339f3e2d
katabathinanithysri <katabathinanithyasri@gmail.com>
2026-04-28 14:49:10 +0530

    added security config

---
+++ src/main/java/com/fsad/backend/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.fsad.backend.config;
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "http://career-guidance-production-329e.up.railway.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
